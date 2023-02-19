b8dca8296cda7661edb92703c139a3e6eb534e66
Nahid Islam <75819596+nahidtislam@users.noreply.github.com>
2023-02-19 12:33:18 +0000

    Initial Commit

---
+++ quick questionnaire composer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  quick questionnaire composer
+//
+//  Created by Nahid Islam on 19/02/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ quick questionnaire composer/quick_questionnaire_composerApp.swift
@@ -0,0 +1,17 @@
+//
+//  quick_questionnaire_composerApp.swift
+//  quick questionnaire composer
+//
+//  Created by Nahid Islam on 19/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct quick_questionnaire_composerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
