425501fb923f973a2d2c6847cca4cd892a8d870e
Nahid Islam <75819596+nahidtislam@users.noreply.github.com>
2023-02-21 07:56:28 +0000

    animated with sprint for answer and added validation on the main add button

--- quick questionnaire composer/AddQuetionsView.swift
+++ quick questionnaire composer/AddQuetionsView.swift
@@ -28,46 +28,64 @@ struct AddQuetionsView: View {
             HStack(spacing: 40) {
                 Button("cancel") {}
                 Button("add") {}
+                    .disabled(!vm.isValid)
             }
         }.padding(.horizontal, 35)
+    }
+    
+    var answerAnimation: Animation {
+        .spring(response: 0.4, dampingFraction: 0.6, blendDuration: 0.2)
     }
     
     var answerBox: some View {
         VStack {
             Button("add answer") {
                 let ans = QuestionCard.Answer(name: "", style: nil, isCorrect: false)
-                withAnimation {
+                withAnimation(answerAnimation) {
                     vm.possibleAnswers.append(ans)
                 }
             }
+//            if vm.possibleAnswers.count == 0 {
+//                Text("your answers are empty")
+//                    .padding(.horizontal, 20)
+//                    .padding(.top, 6)
+//            }
             ScrollView {
                 ForEach($vm.possibleAnswers) { i in
                     answer(item: i)
                         .id(i.id)
                 }
             }
-            .frame(height: 40 * CGFloat(min(6, vm.possibleAnswers.count)))
+            .frame(height: 88 * CGFloat(min(4, vm.possibleAnswers.count)))
             Button("delte answer") {
-                withAnimation {
+                withAnimation(answerAnimation) {
                     _ = vm.possibleAnswers.removeLast()
                 }
             }
+            .disabled(vm.possibleAnswers.count == 0)
         }
-        .padding(6)
-        .background(Color(hex: "#99DDFF"))
+        .padding()
+        .background(Color(hex: "#A9DFFF"))
         .cornerRadius(20)
     }
     
     func answer(item: Binding<QuestionCard.Answer>) -> some View {
-        HStack {
-            item.wrappedValue.shape
-                .resizable()
-                .frame(width: 20, height: 20)
-            TextField("answer name", text: item.name)
-            Spacer()
+        VStack {
+            HStack {
+                item.wrappedValue.shape
+                    .resizable()
+                    .foregroundColor(item.wrappedValue.color)
+                    .frame(width: 20, height: 20)
+                TextField("answer name", text: item.name)
+            }
+            .transition(.move(edge: .bottom))
             Toggle("is correct", isOn: item.isCorrect)
         }
-        .transition(.move(edge: .bottom))
+        .padding(8)
+        .background(Color(hex: "#99EEBB"))
+        .cornerRadius(6)
+        .padding(2)
+        
     }
 }
 
@@ -95,6 +113,10 @@ extension AddQuetionsView {
             }  else {
                 return nil
             }
+        }
+        
+        var isValid: Bool {
+            possibleAnswers.count > 1 && possibleAnswers.first(where: { $0.isCorrect }) != nil
         }
         
         var questionOutput: QuestionCard {
